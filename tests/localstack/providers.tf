acfe3e326a01bfd48a93afa44548a2f7ff8e42d2
jtzero ? <jtzero511@gmail.com>
2026-01-30 12:59:33 -0500

    init localstack testing

---
+++ tests/localstack/providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.3.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.67.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3  = "http://localhost:4566"
+    iam = "http://localhost:4566"
+  }
+}
